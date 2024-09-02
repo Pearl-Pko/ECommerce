23dc59f90189e78833e48a9bb441c9d9b0a88ac1
Pearl-Pko <osamuede75@gmail.com>
2024-09-02 02:38:17 +0100

    Reset image url

--- src/data/data.ts
+++ src/data/data.ts
@@ -9,14 +9,14 @@ export const productsData = [
     {
         "id": 2,
         "name": "HP Stream 11",
-        "url": "https://m.media-amazon.com/images/I/71QUV8SPLNL._AC_UF894,1000_QL80_FMwebp_.jpg",
+        "url": "https://i.pcmag.com/imagery/reviews/05r7grG5jUF6QJXZlqKnmiH-1.fit_lim.size_1200x630.v1699031377.jpg",
         "price": 3000,
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         "id": 3,
         "name": "HP 15",
-        "url": "https://m.media-amazon.com/images/I/813Rrl+kASL._AC_SL1500_.jpg",
+        "url": "https://i.pcmag.com/imagery/reviews/05r7grG5jUF6QJXZlqKnmiH-1.fit_lim.size_1200x630.v1699031377.jpg",
         "price": 1200,
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
@@ -30,14 +30,14 @@ export const productsData = [
     {
         "id": 5,
         "name": "Asus Zenbook 14x OLED",
-        "url": "https://f.media-amazon.com/images/I/61vwwf3hEOL._AC_SY355_.jpg",
+        "url": "https://i.pcmag.com/imagery/reviews/05r7grG5jUF6QJXZlqKnmiH-1.fit_lim.size_1200x630.v1699031377.jpg",
         "price": 700,
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         "id": 6,
         "name": "XPS 15 9500",
-        "url": "https://f.media-amazon.com/images/I/61vwwf3hEOL._AC_SY355_.jpg",
+        "url": "https://i.pcmag.com/imagery/reviews/05r7grG5jUF6QJXZlqKnmiH-1.fit_lim.size_1200x630.v1699031377.jpg",
         "price": 600,
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
